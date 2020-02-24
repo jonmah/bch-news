429dad2581e9836af91656c6d16b2926115e66a6
Jon Mah <jonmah@MLT077569.local>
2020-02-24 08:45:38 -0800

    + api for FeaturePrice

--- src/api/bitcoin.js
+++ src/api/bitcoin.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 export const newsUrl = 'https://www.bitcoin.com/special/news.json';
 
-export const baseUrl = 'https://index-api.bitcoin.com/api/v0/cash/';
-export const historyUrl = `${baseUrl}history`;
+export const baseUrl = 'https://index-api.bitcoin.com/api/v0/';
+export const historyUrl = `${baseUrl}cash/history`;
+export const priceUrl = `${baseUrl}price/usd`;
 
 export const getNews = async () => {
   const result = await axios.get(newsUrl);
@@ -12,5 +13,10 @@ export const getNews = async () => {
 
 export const getPriceHistory = async () => {
   const result = await axios.get(historyUrl);
+  return result.data;
+};
+
+export const getFeaturePrice = async () => {
+  const result = await axios.get(priceUrl);
   return result.data;
 };
